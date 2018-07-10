59a19c8697f9fc55228464eff390d5c209cd9539
Tiago Ribeiro <tribeiros@indracompany.com>
2018-07-10 13:24:57 -0300

    update

---
+++ if.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if [ ! -d $1 ]
+then
+  mkdir $1
+fi
--- tg.sh
+++ tg.sh
@@ -3,23 +3,24 @@
 #tribeiros@indracompany.com
 
 #Variables 
-MSG=$2 #message
+#chatid
+MSG=$1 #message
 botToken='bot572938288:AAGFrgJIdj6dDiPgIgMDuCSNK5VtsBa7O4U' #xalmxbot
-#chatId='-272973710' #indra group  
-#chatId='-242284498' #xansiblex group  
+#chatId='-272973710' #xansiblex group  
+chatId='-242284498' #indra group  
 
 #Validating user arguments
-  if [ ! $2 ]; then
-    echo "message is missing"
-    exit
-  fi
+#if [ ! $1 ]; then
+#  echo "missing"
+#  exit
+#fi
 
 #validating groups
-  if [ $1 -eq 'indra']; then
-    export chatId='-272973710'
-  else
-     export chatId='-242284498'
-  fi
+#  if [ "$CID" -eq "$INDRA" ]; then
+#    export CID='-272973710'
+#  else
+#     export CID='-242284498'
+#  fi
 
 #exec
-curl -X POST "https://api.telegram.org/${botToken}/sendMessage" -d "chat_id=${chatId}&text=${MSG}" > /dev/null
+curl -s -X POST "https://api.telegram.org/${botToken}/sendMessage" -d "chat_id=${chatId}&text=${MSG}" > /dev/null
