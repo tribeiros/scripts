455727455b00b7ef3ca95e7d779c72792c8f916c
Tiago Ribeiro <tribeiros@indracompany.com>
2018-07-11 14:49:32 -0300

    rmlog

---
+++ rmlog.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# remove log from brtlvlts0408co
+# tribeiros@indracompany.com - 07/2018
+
+# Path to clean
+directories=(/bloqspee/bloqueio_desbloqueio/resposta/ /bloqspee/bloqueio_desbloqueio/solicitacao/) #PROD
+devDirectories=(/home/tribeiros/Projects/scripts/rmlogr/ /home/tribeiros/Projects/scripts/rmlogs/) #DEV
+
+days=+7 # argument for `find -mtime`, plus sign indicate to verify files after a number of days, minus sign to before
+
+# PROD function to verify directory and execute rm
+rmlog(){
+  for directory in ${directories[*]}
+    do
+      if [ -d $directory ];then
+        oldFiles=`find $directory* -mtime $days`
+        rm -f $oldFiles
+      else
+        echo $directory not found
+      fi
+    done
+}
+
+# DEV function to verify directory and execute rm
+devrmlog(){
+  for directory in ${devDirectories[*]}
+  do
+    if [ -d $directory ];then
+      oldFiles=`find $directory* -mtime $days`
+      rm -f $oldFiles
+    else
+      echo $directory not found
+    fi
+  done
+}
+
+#execute
+devrmlog
+#rmlog
--- uspeedy.sh
+++ uspeedy.sh
@@ -2,22 +2,23 @@
 # remove log from uspeedy
 # tribeiros@indracompany.com - 07/2018
 
-# Path
+# Path to clean
 dirR='/home/tribeiros/Projects/scripts/rmlogr'
 dirS='/home/tribeiros/Projects/scripts/rmlogs'
 
-#argument for mtime
-days='+7'
+# argument for mtime
+days=+7 
+# plus sign indicate to verify files after a number of days, minus sign to before
 
 # verify directory and execute rm
 if [ -d $dirR ] && [ -d $dirS ];then
   for filesR in `find $dirR/* -mtime $days`
   do
-    rm -rf "$filesR"
+    rm -rf $filesR
   done
   for filesS in `find $dirS/* -mtime $days`
   do
-    rm -rf "$filesS"
+    rm -rf $filesS
   done
 else
   echo 'Directory not found'
