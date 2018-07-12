52f9b150783dd4d52e8aa4ef3cd25ffe88a09c13
Tiago Ribeiro <tribeiros@indracompany.com>
2018-07-12 15:35:36 -0300

    comment

--- tg.sh
+++ tg.sh
@@ -3,21 +3,23 @@
 #tribeiros@indracompany.com
 
 #Variables 
-URL=https://api.telegram.org/
-botToken=bot535199740:AAGhxdpQz5XDW8oSD8JYdpE0aRdXdUZwRrU #xtgxbot
-ansibleGroup=(-272973710 ansible)
-indraGroup=(-242284498 indra)
-testGroup=(-220750192 test)
-GROUP=$1
-MSG=$2 #message
+URL=https://api.telegram.org/ #telegram uri
+botToken=bot535199740:AAGhxdpQz5XDW8oSD8JYdpE0aRdXdUZwRrU #xtgxbot made with botfather
+ansibleGroup=(-272973710 ansible) #array with chat_id and name
+indraGroup=(-242284498 indra) #array with chat_id and name
+testGroup=(-220750192 test) #array with chat_id and name
+GROUP=$1 # first argument for group
+MSG=$2 # second argument for message
 
 #Validating user arguments
+#here we test if all arguments are defined
 if [[ ! $2 ]]; then
   echo "missing arguments"
   exit
 fi
 
 #validating groups
+# function validate names of groups and define each for curl command
 sendMessage() {
 if [[ "$GROUP" == *"${testGroup[1]}"* ]] ;then
   chatId=${testGroup[0]}
